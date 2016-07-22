7944a83745eb910db49d2e4fc5e9793d230d498f
Zac Cobb <zacrobat55@gmail.com>
2016-07-22 13:21:13 +1000

    Set up pages and navigation

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -6,26 +6,26 @@ return!0}function Q(a,b,d,e){if(m.acceptData(a)){var f,g,h=m.expando,i=a.nodeTyp
 
 $(document).ready(function() {
 
-  function myFunction() {
-    document.getElementsByClassName("header ul")[0].classList.toggle("responsive");
-  }
+  // function myFunction() {
+  //   document.getElementsByClassName("header ul")[0].classList.toggle("responsive");
+  // }
 
-  $('header nav ul a').not('.burger').on('click', function(event) {
-    // event.preventDefault();
-    console.log('Ignoring default event!');
-    var linkText = $(this).text();
-    var linkHTML = $(this).parent().html();
-    var linkURL = $(this).attr('href');
-    console.log('You clicked: ' + linkText);
-    console.log(linkHTML);
-    console.log(linkURL);
-    $('header nav ul a').removeClass('active');
-    $(this).addClass('active');
-    // $(this).addClass('active').parent('li').siblings('li').find('a').removeClass('active');
-  });
-  $('header nav ul a').not('.burger').on('click', function(event) {
-
-  });
+  // $('header nav ul a').not('.burger').on('click', function(event) {
+  //   // event.preventDefault();
+  //   console.log('Ignoring default event!');
+  //   var linkText = $(this).text();
+  //   var linkHTML = $(this).parent().html();
+  //   var linkURL = $(this).attr('href');
+  //   console.log('You clicked: ' + linkText);
+  //   console.log(linkHTML);
+  //   console.log(linkURL);
+  //   $('header nav ul a').removeClass('active');
+  //   $(this).addClass('active');
+  //   // $(this).addClass('active').parent('li').siblings('li').find('a').removeClass('active');
+  // });
+  // $('header nav ul a').not('.burger').on('click', function(event) {
+  //
+  // });
 
   $('header .icon a').on('click', function() {
     $('header ul').toggleClass('responsive');
