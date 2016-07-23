bff8cc017cc4ec2f1bc44e35b88292987bea5ca2
Zac Cobb <zacrobat55@gmail.com>
2016-07-23 14:18:27 +1000

    Move vanilla js outside of .ready(function()

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -10,6 +10,10 @@ $(document).ready(function() {
   //   document.getElementsByClassName("header ul")[0].classList.toggle("responsive");
   // }
 
+  $('header .icon a').on('click', function() {
+    $('header ul').toggleClass('responsive');
+  });
+
   // $('header nav ul a').not('.burger').on('click', function(event) {
   //   // event.preventDefault();
   //   console.log('Ignoring default event!');
@@ -27,29 +31,25 @@ $(document).ready(function() {
   //
   // });
 
-  $('header .icon a').on('click', function() {
-    $('header ul').toggleClass('responsive');
-  });
+});
 
-  function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
 
-});
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
