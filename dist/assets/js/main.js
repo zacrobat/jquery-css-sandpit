f1e244a135f206ca672b7e869d0b33bf94494570
Zac Cobb <zacrobat55@gmail.com>
2016-08-12 12:11:35 +1000

    Update tab js - more DRY

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -5,25 +5,12 @@ return!0}function Q(a,b,d,e){if(m.acceptData(a)){var f,g,h=m.expando,i=a.nodeTyp
 ;
 
 $(document).ready(function() {
-  $('#tab01').click(function() {
-    $('#tabcontent01').siblings().addClass('invisible');
-    $('#tabcontent01').removeClass('invisible').addClass('visible');
-  });
-  $('#tab02').click(function() {
+  $('.tabs a').on('click', function() {
     $('ul.tabs li a').removeClass('active-tab');
-    $('#tabcontent02').siblings().addClass('invisible');
-    $('#tabcontent02').removeClass('invisible').addClass('visible');
-  });
-  $('#tab03').click(function() {
-    $('ul.tabs li a').removeClass('active-tab');
-    $('.tab').siblings().addClass('inactive-tab');
-    $('#tabcontent03').siblings().addClass('invisible');
-    $('#tabcontent03').removeClass('invisible').addClass('visible');
-  });
-  $('#tab04').click(function() {
-    $('ul.tabs li a').removeClass('active-tab');
-    $('.tab').siblings().addClass('inactive-tab');
-    $('#tabcontent04').siblings().addClass('invisible');
-    $('#tabcontent04').removeClass('invisible').addClass('visible');
+    var name = $(this).attr('for');
+    console.log(name);
+    var completeName = '[name="' + name + '"]';
+    console.log(completeName);
+    $(completeName).removeClass('invisible').addClass('visible').siblings().removeClass('visible').addClass('invisible');
   });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,10 @@
 $(document).ready(function() {
-  $('#tab01').click(function() {
-    $('#tabcontent01').siblings().addClass('invisible');
-    $('#tabcontent01').removeClass('invisible').addClass('visible');
-  });
-  $('#tab02').click(function() {
+  $('.tabs a').on('click', function() {
     $('ul.tabs li a').removeClass('active-tab');
-    $('#tabcontent02').siblings().addClass('invisible');
-    $('#tabcontent02').removeClass('invisible').addClass('visible');
-  });
-  $('#tab03').click(function() {
-    $('ul.tabs li a').removeClass('active-tab');
-    $('.tab').siblings().addClass('inactive-tab');
-    $('#tabcontent03').siblings().addClass('invisible');
-    $('#tabcontent03').removeClass('invisible').addClass('visible');
-  });
-  $('#tab04').click(function() {
-    $('ul.tabs li a').removeClass('active-tab');
-    $('.tab').siblings().addClass('inactive-tab');
-    $('#tabcontent04').siblings().addClass('invisible');
-    $('#tabcontent04').removeClass('invisible').addClass('visible');
+    var name = $(this).attr('for');
+    console.log(name);
+    var completeName = '[name="' + name + '"]';
+    console.log(completeName);
+    $(completeName).removeClass('invisible').addClass('visible').siblings().removeClass('visible').addClass('invisible');
   });
 });
