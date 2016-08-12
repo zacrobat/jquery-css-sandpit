f63190fd259ba971dfce2f64149b1ffa9663c908
Zac Cobb <zacrobat55@gmail.com>
2016-08-12 16:17:50 +1000

    Refactor jQuery tabs to remove unnecessary html id's and classes

--- src/js/main.js
+++ src/js/main.js
@@ -5,17 +5,4 @@ $(document).ready(function() {
     var completeName = '[name="' + $(this).attr('for') + '"]';
     $(completeName).show().siblings().hide();
   });
-  // $('.tabs a').on('click', function() {
-  //   $('ul.tabs li a').removeClass('active-tab');
-  //   var completeName = '[name="' + $(this).attr('for') + '"]';
-  //   $(completeName).removeClass('invisible').addClass('visible').siblings().removeClass('visible').addClass('invisible');
-  // });
-  // $('.tabs a').on('click', function() {
-  //   $('ul.tabs li a').removeClass('active-tab');
-  //   var name = $(this).attr('for');
-  //   console.log(name);
-  //   var completeName = '[name="' + name + '"]';
-  //   console.log(completeName);
-  //   $(completeName).removeClass('invisible').addClass('visible').siblings().removeClass('visible').addClass('invisible');
-  // });
 });
